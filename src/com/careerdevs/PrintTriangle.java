54b2d780bcbfdc96179cf840dd9acea865808cc2
JohnPena0115 <calmwinds06@gmail.com>
2022-01-17 23:39:32 -0500

    "Uploading directory along with PrintTriangle"

---
+++ src/com/careerdevs/PrintTriangle.java
@@ -0,0 +1,55 @@
+package com.careerdevs;
+
+public class PrintTriangle {
+
+    public static void main(String[] args) {
+        // The tests are not checking the main, so you can modify it freely.
+
+        printTriangle(5);
+
+        System.out.println("------------------------------");
+
+        printTriangle(10);
+
+    }
+
+    public static void printStars(int number) {
+
+        for (int i = 0; i < number; i++){
+
+            System.out.print("*");
+        }
+
+        System.out.println("");
+    }
+
+    public static void printSpaces(int number) {
+
+        for (int i = 0; i < number; i++){
+
+            System.out.print(" ");
+        }
+    }
+
+    public static void printTriangle(int size) {
+
+        int spaces = size -1;
+        int stars = 1;
+
+        while (size >= 1) {
+
+            if (spaces != 0) {
+
+                printSpaces(spaces);
+            }
+
+            printStars(stars);
+
+            size--;
+            spaces--;
+            stars++;
+
+        }
+    }
+
+}
